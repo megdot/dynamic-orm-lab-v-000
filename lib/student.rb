838766e5d129a5ddc4456f8876f7895ceaa9b98b
Megan Dotto <megan.dotto@gmail.com>
2018-06-09 02:50:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,28 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql="PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |attributes, value|
+      self.send("#{attributes}=", value)
+    end
+  end
+
+
 end
